1f1533b68f585c79b211af93408e8a6f297c2843
Michael Stone <m@mstone.info>
2022-12-09 14:12:38 -0500

    update source

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use dioxus::prelude::*;
+
+pub struct AppProps {}
+
+pub fn app(cx: Scope<AppProps>) -> Element {
+    cx.render(rsx!{
+        div {
+            "Hello, world!"
+        }
+    }
 }
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
+    console_error_panic_hook::set_once();
+
+    dioxus_web::launch_with_props(
+        app,
+        AppProps {},
+        dioxus_web::Config::new()
+    );
+}
+
+
